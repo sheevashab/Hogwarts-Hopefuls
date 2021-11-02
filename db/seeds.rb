8f19b15c8411458fcd1b6d4038143ca018da650c
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-11-02 18:48:45 -0500

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,6 @@ User.destroy_all
 
 puts "#{User.count} users created"
 
-@harry = Student.create!(pet: 'owl', patronus: 'stag', img_url: 'http://aribrenner.com/media/images/ari0.jpg', user: @admin, house: @slytherine, spell: @lumos)
-@hermione = Student.create!(pet: 'cat', patronus: 'otter', img_url: 'http://i.imgur.com/JRkmvOv.png', user: @admin, house: @ravenclaw, spell: @wingardiumleviosa)
-@ron = Student.create!(pet: 'rat', patronus: 'can I even do this spell', img_url: 'https://avatars0.githubusercontent.com/u/12768542?v=4&s=400', user: @admin, house: @hufflepuff, spell: @wingardiumleviosa)
-@neville = Student.create!(pet: 'toad', patronus: 'non-corporeal', img_url: 'https://docs.npmjs.com/images/npm.svg', user: @admin, house: @gryffindor, spell: @reparo)
-@malfoy = Student.create!(pet: 'Snape', patronus: 'I am incapable', img_url: 'http://uploads.tapatalk-cdn.com/20161123/679e78cd8a276f76f1f962ba2a4c454e.jpg', user: @admin, house: @slytherine, spell: @incendio)
-
-puts "#{Student.count} students created"
-
 @ravenclaw = House.create!(name: 'Ravenclaw', description: 'smarties')
 @hufflepuff = House.create!(name: 'Hufflepuff', description: 'sweeties')
 @slytherine = House.create!(name: 'Slytherine', description: 'getters')
@@ -30,3 +22,11 @@ puts "#{House.count} houses created"
 @lumos = Spell.create!(name: 'Lumos', description: 'light')
 
 puts "#{Spell.count} spells created"
+
+@harry = Student.create!(pet: 'owl', patronus: 'stag', img_url: 'http://aribrenner.com/media/images/ari0.jpg', user: @admin, house: @slytherine, spell: @lumos)
+@hermione = Student.create!(pet: 'cat', patronus: 'otter', img_url: 'http://i.imgur.com/JRkmvOv.png', user: @admin, house: @ravenclaw, spell: @wingardiumleviosa)
+@ron = Student.create!(pet: 'rat', patronus: 'can I even do this spell', img_url: 'https://avatars0.githubusercontent.com/u/12768542?v=4&s=400', user: @admin, house: @hufflepuff, spell: @wingardiumleviosa)
+@neville = Student.create!(pet: 'toad', patronus: 'non-corporeal', img_url: 'https://docs.npmjs.com/images/npm.svg', user: @admin, house: @gryffindor, spell: @reparo)
+@malfoy = Student.create!(pet: 'Snape', patronus: 'I am incapable', img_url: 'http://uploads.tapatalk-cdn.com/20161123/679e78cd8a276f76f1f962ba2a4c454e.jpg', user: @admin, house: @slytherine, spell: @incendio)
+
+puts "#{Student.count} students created"
