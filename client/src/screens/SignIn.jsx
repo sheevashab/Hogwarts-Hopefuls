caf19dfaea027d512c2fffac2b9b67d0e1a497b8
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-11-03 13:48:38 -0500

    sign up and sign in components rendered

--- client/src/screens/SignIn.jsx
+++ client/src/screens/SignIn.jsx
@@ -1,7 +1,58 @@
-export default function SignIn() {
-  return (
-    <div>
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-    </div>
+export default function SignIn(props) {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+  const { username, password } = formData;
+  const { handleSignIn } = props;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+
+
+  return (
+    <form
+      className='sign-in-form'
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSignIn(formData);
+      }}>
+      <h3>Welcome Back</h3>
+      <h4>To The Wonderful World Of Witchcraft And Wizardry</h4>
+      <label>
+        Username:
+        <input
+          type='text'
+          name='username'
+          value={username}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type='password'
+          name='password'
+          value={password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button>
+        <Link to='/'>
+          Sign In</Link></button>
+      <h5>Haven't received your Hogwarts Letter yet?</h5>
+      <Link to='/sign-up'>Sign Up</Link>
+    </form>
   )
 }
--- client/src/screens/SignUp.jsx
+++ client/src/screens/SignUp.jsx
@@ -1,7 +1,61 @@
-export default function SignUp() {
-  return (
-    <div>
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-    </div>
-  )
+export default function SignUp(props) {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const { username, email, password } = formData;
+  const { handleSignUp } = props;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSignUp(formData);
+      }}
+    >
+      <h3>Create Account</h3>
+      <h4>To Join The Wonderful World Of Witchcraft And Wizardry</h4>
+      <label>
+        Username:
+        <input
+          type='text'
+          name='username'
+          value={username}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type='text' name='email' value={email} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type='password'
+          name='password'
+          value={password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button>
+        <Link to='/'>Sign Up</Link></button>
+      <h5>Already received your Hogwarts letter?</h5>
+      <Link to='/sign-up'>Sign In</Link>
+    </form>
+  );
 }
