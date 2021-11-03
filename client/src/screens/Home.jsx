723b76ed7d4a0721ab53b8d4b7d4466081fe417f
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-11-03 16:24:17 -0500

    Home and Letter component functionality

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
-    <div>
-      This is home
-    </div>
+    <>
+      <div>Welcome to the Wonderful World of Witchcraft and Wizardry!</div>
+      <img src='https://res.cloudinary.com/dyyjvyqtn/image/upload/v1635973772/deathlyhallows_fd4ur1.png' />
+      <div>Hogwarts Hopefuls provides  magical beings like yourself a platform to share your supernatural persona</div>
+      <img src='https://res.cloudinary.com/dyyjvyqtn/image/upload/v1635973772/deathlyhallows_fd4ur1.png' />
+      <div>Sign Up To Get Sorted Into Your Hogwarts House And To Reveal Your Specialty Spell</div>
+      <button>
+        <Link to='/sign-up'>Sorting Hat Picture</Link>
+      </button>
+    </>
   )
 }
--- client/src/screens/Letter.jsx
+++ client/src/screens/Letter.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Letter() {
   return (
-    <div>
-      This is letter
-    </div>
+    <>
+      <p>
+        Dear ****Hermione’s Twin******,<br />
+        We are pleased to inform you that you have been accepted to Hogwarts School of Witchcraft and Wizardry.
+        We very much look forward to receiving you as part of the new generation of Hogwarts.<br />
+        Yours Sincerely,<br />
+        Minerva McGonagall<br />
+        As with most years at this school, something is amiss. As such, we will be sorting you before your arrival on the Hogwarts Express. Please click your wand on the magical link below to connect your body and spirit to the Sorting Hat.
+      </p>
+      <button>
+        <Link to='/sorting'>Sorting Hat Picture</Link>
+      </button>
+    </>
   )
 }
