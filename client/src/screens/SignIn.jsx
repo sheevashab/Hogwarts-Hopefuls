ca5412bacf0b7b86b6edfa954c49a449463c3559
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-11-04 15:27:43 -0500

    styling SignIn screen

--- client/src/screens/SignIn.jsx
+++ client/src/screens/SignIn.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import './SignIn.css'
+
 export default function SignIn(props) {
   const [formData, setFormData] = useState({
     username: '',
@@ -20,37 +22,39 @@ export default function SignIn(props) {
 
 
   return (
-    <form
-      className='sign-in-form'
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSignIn(formData);
-      }}>
-      <h3>Welcome Back</h3>
-      <h4>To The Wonderful World Of Witchcraft And Wizardry</h4>
-      <label>
-        Username:
-        <input
-          type='text'
-          name='username'
-          value={username}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type='password'
-          name='password'
-          value={password}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <button>Sign In</button>
-      <h5>Haven't received your Hogwarts letter yet?</h5>
-      <Link to='/sign-up'>Sign Up</Link>
-    </form>
+    <div className='sign-in-container'>
+      <form
+        className='sign-in-form'
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSignIn(formData);
+        }}>
+        <h3 className='sign-in-intro'>Welcome Back</h3>
+        <h4 className='sign-in-message'>To The Wonderful World Of Witchcraft And Wizardry</h4>
+        <label>
+          username
+          <input
+            type='text'
+            name='username'
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          password
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button>Sign In</button>
+        <h5>Haven't received your Hogwarts letter yet?</h5>
+        <Link to='/sign-up'><button>Sign Up</button></Link>
+      </form>
+    </div>
   )
 }
