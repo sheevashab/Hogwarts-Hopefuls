8c881700635883c220ef112fba8d7e31d277b30b
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-11-07 16:29:18 -0600

    Layout Navbar Footer styling with media queries

--- client/src/componenets/Layout.jsx
+++ client/src/componenets/Layout.jsx
@@ -4,15 +4,9 @@ import Footer from './Footer';
 export default function Layout(props) {
   return (
     <div className='layout'>
-      {/* <div className='layout-navbar'> */}
       <Navbar currentUser={props.currentUser} handleSignOut={props.handleSignOut} />
-      {/* </div> */}
-      {/* <div className='layout-children'> */}
       {props.children}
-      {/* </div>
-      <div className='layout-footer'> */}
       <Footer />
-      {/* </div> */}
     </div>
   )
 }
